a640665c79111150d390080e018468d92a9fefb9
Franciz Emmanuelle Angelo Esperanza <franciz_esperanza@dlsu.edu.ph>
2026-03-10 22:00:07 +0800

    finished login page design

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Login from './Login.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Router>
+    <Routes>
+      <Route path="*" element={<Navigate replace to="/login" />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </Router>,
 )
