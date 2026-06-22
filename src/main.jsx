021dc63381ee6e3dd88e2bd9318f6ceea2b6a9f1
Franciz Emmanuelle Angelo Esperanza <franciz_esperanza@dlsu.edu.ph>
2026-06-22 16:28:59 +0800

    added tan stack query

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom'
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import './index.css'
 import Login from './Login.jsx'
 import SignUp from './SignUp.jsx'
@@ -17,6 +18,7 @@ import axios from 'axios';
 
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;
+const queryClient = new QueryClient();
 
 function DefaultRedirect() {
   const { user } = useAuth();
@@ -26,7 +28,8 @@ function DefaultRedirect() {
 
 createRoot(document.getElementById('root')).render(
   <>
-    <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
         <Router>
           <Routes>
             <Route path="/login" element={<Login />} />
@@ -57,7 +60,9 @@ createRoot(document.getElementById('root')).render(
             <Route path="*" element={<DefaultRedirect />} />
           </Routes>
         </Router>
-    </AuthProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+    
     <Toaster />
   </>
   
